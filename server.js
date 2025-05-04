e56700dbad5f8f93c95cd69aa963a4bf2024f95d
Sotto1234-max <mrsotto05@gmail.com>
2025-05-04 23:18:51 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,35 +6,59 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-let users = [];
+const users = {}; // { username: socketId }
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('User connected:', socket.id);
 
-  socket.on('new-user', (userInfo) => {
-    userInfo.id = socket.id;
-    users.push(userInfo);
-    io.emit('user-list', users);
+  // Handle new user joining
+  socket.on('new-user', user => {
+    users[user.name] = socket.id;
+
+    // Send updated user list to all clients
+    const userList = Object.entries(users).map(([name, id]) => ({
+      name,
+      socketId: id,
+      ...user, // include other user data like gender, age, country, etc.
+    }));
+    io.emit('user-list', userList);
   });
 
+  // Handle sending message to a specific user
   socket.on('send-message', ({ to, message }) => {
-    const receiver = users.find(user => user.name === to);
-    if (receiver) {
-      io.to(receiver.id).emit('receive-message', {
-        from: users.find(u => u.id === socket.id)?.name || 'Unknown',
-        message
+    const receiverSocketId = users[to];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit('receive-message', {
+        from: getUsernameBySocketId(socket.id),
+        message,
       });
     }
   });
 
+  // Handle user disconnection
   socket.on('disconnect', () => {
-    users = users.filter(user => user.id !== socket.id);
-    io.emit('user-list', users);
+    const name = getUsernameBySocketId(socket.id);
+    if (name) {
+      delete users[name];
+      io.emit('user-list', Object.entries(users).map(([name, id]) => ({
+        name,
+        socketId: id,
+        // Include any other data if needed
+      })));
+    }
+    console.log('User disconnected:', socket.id);
   });
+
+  // Function to get username by socketId
+  function getUsernameBySocketId(id) {
+    return Object.keys(users).find(name => users[name] === id);
+  }
 });
 
+// Serve static files from 'public' directory
 app.use(express.static('public'));
 
+// Start the server
 server.listen(3000, () => {
   console.log('Server listening on http://localhost:3000');
 });
